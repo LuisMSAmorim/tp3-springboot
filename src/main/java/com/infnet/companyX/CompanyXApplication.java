a4aaa9b7da31e7748c861decda71cd9401e92fba
luisMSAmorim <luismiguelsilvaamorim@gmail.com>
2025-09-11 13:57:17 -0300

    first commit

---
+++ src/main/java/com/infnet/companyX/CompanyXApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.companyX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyXApplication.class, args);
+	}
+
+}
